f01ae7ffadef3d177e135b2e9e620d03b4c85c40
Saroj Padhan <40264980+saroj8455@users.noreply.github.com>
2023-11-11 17:48:49 +0530

    apply form validation with field

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+  styleUrls: ['./contact.component.css'],
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  contactGroup = new FormGroup({
+    email: new FormControl('',[Validators.required,Validators.email]),
+    password: new FormControl(''),
+    address: new FormControl(''),
+    address2: new FormControl(''),
+    city: new FormControl(''),
+    state: new FormControl(''),
+    zip: new FormControl(''),
+    checkout:new FormControl(true)
+  });
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  createContact(form:FormGroup){
+    console.log(form);
+
+
+  }
 }
